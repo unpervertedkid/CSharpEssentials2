fb72ac17fd8804f2096b853f00844f62485bfa0f
Brian Silah <unpervertedkid@gmail.com>
2023-04-25 12:16:14 +0300

    Working with generics

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,5 @@
-﻿using Generics;
+﻿using System.Text.Json;
+using Generics;
 
 Person person1 = new Person("John", "Doe", "21");
 Person person2 = new Person("Jane", "Doe", "22");
@@ -7,6 +8,16 @@ Swap(ref person1, ref person2);
 
 Console.WriteLine(person1.FirstName);
 Console.WriteLine(person2.FirstName);
+
+string jsonPerson = @"{""FirstName"":""Peter"",""LastName"":""Doe"",""Age"":""21""}";
+
+var person3 = JsonSerializer.Deserialize<Person>(jsonPerson);
+
+Console.WriteLine(person3?.FirstName);
+
+Nullable<DateTime> maybeDate = null;
+
+Console.WriteLine(maybeDate.GetValueOrDefault());
 
 void Swap<T>(ref T person1, ref T person2)
 { 
