0f90a4d27aea65ca56172fd2419538a78a1a2b2d
Brian Silah <unpervertedkid@gmail.com>
2023-04-25 12:06:13 +0300

    Understanding generics

---
+++ Generics/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Generics;
+
+public class Person
+{
+    public Person(string firstName, string lastName, string age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Age { get; set; }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿using Generics;
+
+Person person1 = new Person("John", "Doe", "21");
+Person person2 = new Person("Jane", "Doe", "22");
+
+Swap(ref person1, ref person2);
+
+Console.WriteLine(person1.FirstName);
+Console.WriteLine(person2.FirstName);
+
+void Swap<T>(ref T person1, ref T person2)
+{ 
+    T temp = person1;
+    person1 = person2;
+    person2 = temp;
+}
+
