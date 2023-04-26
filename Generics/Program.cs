8133d0690ffb1ab1734523a1256e9587d542df04
Brian Silah <unpervertedkid@gmail.com>
2023-04-26 12:24:53 +0300

    Understanding collection types in the BCL

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,36 +1,13 @@
-﻿using Generics;
-using Library;
+﻿string[] names = new string[2];
+names[0] = "John";
+names[1] = "Mary";
 
-var c = new Customer()
+Console.WriteLine("Hello {0} {1}", names);
+
+var planets = new System.Collections.ArrayList(4);
+planets.AddRange(new string[] { "Mercury", "Venus", "Earth", "Mars" , "Jupiter", "Saturn", "Uranus", "Neptune" });
+
+foreach (var planet in planets)
 {
-    Id = 1,
-    FirstName = "John",
-    LastName = "Doe",
-    Age = 30,
-    Address = "123 Main St"
-};
-
-var c2 = new Customer()
-{
-    Id = 2,
-    FirstName = "Petter",
-    LastName = "Griffin",
-    Age = 25,
-    Address = "123 Main St"
-};
-
-var mapper = new CustomerToPersonMapper();
-var p = c.Map<Person>(mapper); //mapper.Map(c));
-
-Console.WriteLine($"First Name: {p.FirstName}");
-
-var sorter = new Sorter<Customer>();
-var customers = new Customer[] { c2, c };
-sorter.Sort(customers);
-
-foreach (var customer in customers)
-{
-    Console.WriteLine($"Customer {customer.Id} : {customer.LastName}");
+    Console.WriteLine(planet);
 }
-
-
